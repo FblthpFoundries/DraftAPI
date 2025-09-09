5e467d9b9dea9a36f1b83f9b20a9d4ed6e7c8093
OWEN JACKSON <u1229070@umail.utah.edu>
2025-09-09 12:22:13 -0600

    forgor to add these with player

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/FblthpFoundries/DraftAPI
 
 go 1.25.0
 
-require github.com/google/uuid v1.6.0 // indirect
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+)
--- server.go
+++ server.go
@@ -4,17 +4,20 @@ import(
 	"fmt"
 	"net/http"
 	"github.com/FblthpFoundries/DraftAPI/room"
+	"github.com/FblthpFoundries/DraftAPI/player"
 )
 
 func main(){
 	mux := http.NewServeMux()
 	rs := room.NewRoomServer()
+	ps := player.NewPlayerServer()
 	mux.HandleFunc("GET /hello", func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprint(w, "World")
 	})
 
 
 	mux.HandleFunc("POST /newRoom", rs.NewRoom)
+	mux.HandleFunc("POST /register", ps.NewPlayer)
 
 	fmt.Println("This Bitch is Serving")
 
